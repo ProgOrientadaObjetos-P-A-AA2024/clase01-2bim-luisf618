e05f81afd6524660e30d45252e3d539a9b5c8e98
Luis Cordova <lcordovacarrion@gmail.com>
2024-06-06 17:25:03 -0500

    Clase

--- EjemploJava/HerenciaUno/src/herencia2/Ejecutor3.java
+++ EjemploJava/HerenciaUno/src/herencia2/Ejecutor3.java
@@ -8,22 +8,31 @@ package herencia2;
 import java.util.Locale;
 import java.util.Scanner;
 import herencia1.EstudianteDistancia;
+import herencia1.EstudiantePresencial;
 
 /**
  *
  * @author reroes
  */
 public class Ejecutor3 {
+
     public static void main(String[] args) {
         // Ingresar por teclado
         // un objeto de tipo Estudiante Distancia si el usuario ingresa 1 como 
         // opción
         // un objeto de tipo Estudiante Presencial si el usuario ingresa 2 como 
         // opción
-        
+
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
+        int opcion;
+
+        System.out.println("Si desea ingresar un estudiante a distancia ingrese"
+                + " (1) y si desea ingresar un estudiante presencial ingrese"
+                + "(2)");
+        opcion = entrada.nextInt();
         System.out.println("Ingrese nombres");
+        entrada.nextLine();
         String nombres = entrada.nextLine();
         System.out.println("Ingrese apellidos");
         String apellidos = entrada.nextLine();
@@ -31,21 +40,42 @@ public class Ejecutor3 {
         String identificacion = entrada.nextLine();
         System.out.println("Ingrese edad");
         int edad = entrada.nextInt();
-        System.out.println("Ingrese número de asignaturas");
-        int asignaturas = entrada.nextInt();
-        System.out.println("Ingrese costo asignatura");
-        double costoAsignatura = entrada.nextDouble();
-        EstudianteDistancia estudiante = new EstudianteDistancia();
-        
-        estudiante.establecerNombresEstudiante(nombres);
-        estudiante.establecerApellidoEstudiante(apellidos);
-        estudiante.establecerEdadEstudiante(edad);
-        estudiante.establecerIdentificacionEstudiante(identificacion);
-        estudiante.establecerNumeroAsginaturas(asignaturas);
-        estudiante.establecerCostoAsignatura(costoAsignatura);
-        estudiante.calcularMatriculaDistancia();
-        
-        System.out.printf("%s\n", estudiante);
-        
+
+        if (opcion == 1) {
+            EstudianteDistancia estudiante = new EstudianteDistancia();
+            System.out.println("Ingrese número de asignaturas");
+            int asignaturas = entrada.nextInt();
+            System.out.println("Ingrese costo asignatura");
+            double costoAsignatura = entrada.nextDouble();
+            estudiante.establecerNumeroAsginaturas(asignaturas);
+            estudiante.establecerCostoAsignatura(costoAsignatura);
+            estudiante.calcularMatriculaDistancia();
+            estudiante.establecerNombresEstudiante(nombres);
+            estudiante.establecerApellidoEstudiante(apellidos);
+            estudiante.establecerEdadEstudiante(edad);
+            estudiante.establecerIdentificacionEstudiante(identificacion);
+            System.out.printf("%s\n", estudiante);
+
+        } else {
+            if (opcion == 2) {
+                EstudiantePresencial estudiante = new EstudiantePresencial();
+                System.out.println("Ingrese número de creditos");
+                int creditos = entrada.nextInt();
+                System.out.println("Ingrese costo asignatura");
+                double costoAsignatura = entrada.nextDouble();
+                estudiante.establecerNumeroCreditos(creditos);
+                estudiante.establecerCostoCredito(costoAsignatura);
+                estudiante.calcularMatriculaPresencial();
+                estudiante.establecerNombresEstudiante(nombres);
+                estudiante.establecerApellidoEstudiante(apellidos);
+                estudiante.establecerEdadEstudiante(edad);
+                estudiante.establecerIdentificacionEstudiante(identificacion);
+                System.out.printf("%s\n", estudiante);
+
+            } else {
+                System.out.println("Opcion incorrecta");
+            }
+        }
+
     }
 }
